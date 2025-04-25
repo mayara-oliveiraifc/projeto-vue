08454c729f457539fde7e67f735b28c69609aea0
Mayara da Silva Oliveira <mayara.oliveiraifc2024@gmail.com>
2025-04-25 14:08:57 -0300

    FEAT: marcacao menu

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,54 @@
 
 <template>
   <header>
-    <h1>Olá, mundo</h1>
+    <nav>
+      <div class="IFbooks">
+        <a href="App.vue">IFbooks</a>
+        <a href="App.vue">Apreço a
+          leitura</a>
+      </div>
+      <div id="divBusca">
+        <input type="text" id="txtBusca" placeholder="Buscar..."/>
+        <img src="#" id="btnBusca" alt="Buscar"/>
+      </div>
+      <div class="menuTopo">
+        <ul>
+          <li>
+            <a href="App.vue">Termos</a>
+          </li>
+          <li>
+            <a href="App.vue">Equpe</a>
+          </li>
+          <li>
+            <a href="App.vue">Envio</a>
+          </li>
+          <li>
+            <a href="App.vue">Devoluções</a>
+          </li>
+        </ul>
+      </div>
+      <div class="icons">
+        <ul>
+          <li>
+            <a href="App.vue">
+              <span class="fa-solid fa-cart-shopping"></span>
+            </a>
+          </li>
+          <li>
+            <a href="App.vue">
+              <span class="fa-solid fa-heart"></span>
+            </a>
+          </li>
+          <li>
+            <a href="App.vue">
+              <span class="fa-solid fa-user-large"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
+
 </template>
 
 <style scoped>
