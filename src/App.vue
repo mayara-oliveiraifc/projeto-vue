5195d16e0bfbf1b391b718c1d2d3ea10200b4e59
Mayara da Silva Oliveira <mayara.oliveiraifc2024@gmail.com>
2025-04-25 15:12:02 -0300

    FEAT: css menu-inicio

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
   <header>
     <nav>
       <div class="IFbooks">
-        <a href="App.vue">IFbooks</a>
-        <a href="App.vue">Apreço a
-          leitura</a>
+       <ul>
+        <li class="espacoLogo" >
+          <a href="App.vue">IFbooks</a>
+        </li>
+        <li>
+          <p class="espaco">Apreço a <br> leitura</p>
+        </li>
+       </ul>
       </div>
-      <div id="divBusca">
+      <div class="barraDePesquisa">
         <input type="text" id="txtBusca" placeholder="Buscar..."/>
         <img src="#" id="btnBusca" alt="Buscar"/>
       </div>
@@ -57,4 +62,71 @@
 
 <style scoped>
 
+  /*ALINHAMENTO MENU*/
+  header nav {
+    display: flex;
+  }
+  header nav div.IFbooks ul{
+    display: flex;
+  }
+  header nav div.menuTopo ul{
+  display: flex;
+  }
+  header nav div.icons ul{
+    display: flex;
+  }
+  /* ESPAÇAMENTO MENU*/ 
+
+  header nav div.IFbooks ul li p.espaco{
+    margin: 20px 4px 10px 0;
+  }
+  header nav div.IFbooks ul li.espacoLogo{
+    margin: 20px 10px 10px 8vw;
+  }
+  header nav div.barraDePesquisa{
+    margin: 20px 10px 10px 6vw;
+  }
+  header nav div.menuTopo ul li a{
+    margin: 23px;
+  }
+  header nav div.menuTopo ul li {
+   margin: 20px 10px 10px 0;
+  }
+  header nav div.icons ul li  {
+    margin: 10px;
+  }
+  header nav div.icons ul{
+    margin: 10px 2vw 10px 0;
+  }
+
+   /*ESTILIZAÇÃO MENU*/
+
+  header nav div.IFbooks ul li.espacoLogo a{
+   text-decoration: none;
+   color: black;
+  }
+  header nav div.menuTopo ul li a{
+   text-decoration: none;
+   color: #7B7881;
+  }
+
+  header nav div.icons ul li a span{
+   color: #27AE60;
+  }
+
+  header nav div.IFbooks ul li p.espaco{
+    color: #27AE6099;
+  }
+
+  /*BORDA MENU*/
+
+  header nav{
+    border-bottom: 1.5px solid #27AE60;
+  }
+
+ 
+  header nav div.IFbooks ul li p.espaco{
+    border-left: 1.5px solid #27AE60;
+    padding: 0 0 0 12px;
+  }
 </style>
